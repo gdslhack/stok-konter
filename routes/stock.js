3dbec406c084e07016589fc4af54194a8ef39605
Dedy Setiadi Pramadan <34924797+gdslhack@users.noreply.github.com>
2024-09-08 16:30:10 +0700

    Update stock.js

--- routes/stock.js
+++ routes/stock.js
@@ -27,4 +27,15 @@ router.post('/stock-out', async (req, res) => {
   }
 });
 
+// Route untuk menampilkan data stock
+router.get('/', async (req, res) => {
+  try {
+    // Mengambil data stok dari database
+    const stocks = await Stock.find().populate('productId');
+    res.render('stock', { title: 'Stock Page', header: 'Stock', stocks });
+  } catch (error) {
+    res.status(500).send('Error retrieving stock data');
+  }
+});
+
 module.exports = router;
